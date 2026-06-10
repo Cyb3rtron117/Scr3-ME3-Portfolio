467eca0c54de3ecadcc5b20bbb6f8d28f0cacbb7
Cyb3rtron <dian.ehrke@gmail.com>
2026-06-10 16:23:21 +0200

    navbar background
    active page has a small active color too
    and tooltips are now on hover only

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl, {
+  trigger: 'hover' // Removes the 'focus' requirement
+}))
+const navbarButtons = $(".nav-link").toArray();
+
+navbarButtons.forEach(element => {
+    $(element).on("click", function() {
+        $(".active").removeClass("active");
+        $(this).addClass("active");   
+    })
+})
