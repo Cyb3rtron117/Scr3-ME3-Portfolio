0c143214cc37a88cf478806f2f6a2e408e0ddc71
Cyb3rtron <dian.ehrke@gmail.com>
2026-06-11 17:02:36 +0200

    homepage
    homepage added
    
    page navigation added
    
    navbar collapses on mobile when buttons clicked or when open different page

--- main.js
+++ main.js
@@ -3,10 +3,60 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
   trigger: 'hover' // Removes the 'focus' requirement
 }))
 const navbarButtons = $(".nav-link").toArray();
+const pages = $(".pages").toArray();
+const currentYear = new Date().getFullYear();
+$("#current_year").text(currentYear);
 
-navbarButtons.forEach(element => {
-    $(element).on("click", function() {
-        $(".active").removeClass("active");
-        $(this).addClass("active");   
-    })
-})
+const navbarCollapseEl = $('#navbarSupportedContent');
+
+function switchNav(button) 
+{
+    $(".active").removeClass("active");
+    $(button).addClass("active");  //makes the correct button look active
+
+    //collapses the navbar on mobile when button is clicked
+    if ($(navbarCollapseEl).hasClass('show')) {
+        const bsCollapse = bootstrap.Collapse.getOrCreateInstance(navbarCollapseEl);            
+        bsCollapse.hide();
+    } 
+}
+
+$("#home_button").on("click", showHome)
+function showHome() 
+{
+    switchNav("#home_button");
+    pages.forEach(element => {    
+        $(element).hide()    
+    });
+    $("#homepage").show();   
+}
+
+$("#about_button").on("click", showAbout)
+function showAbout() 
+{
+    switchNav("#about_button");
+    pages.forEach(element => {    
+        $(element).hide()    
+    });
+    $("#about_page").show();   
+}
+
+$("#portfolio_button").on("click", showPortfolio)
+function showPortfolio() 
+{
+    switchNav("#portfolio_button");
+    pages.forEach(element => {    
+        $(element).hide()    
+    });
+    $("#portfolio_page").show();   
+}
+
+$("#contact_button").on("click", showContact)
+function showContact() 
+{
+    switchNav("#contact_button");
+    pages.forEach(element => {    
+        $(element).hide()    
+    });
+    $("#contact_page").show();   
+}
